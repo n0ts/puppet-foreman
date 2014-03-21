b7779d12a4dd17607c834069fb1a8d5e9b4a8181
Charlie Somerville <charlie@charliesomerville.com>
2014-03-21 15:38:30 +1100

    simplify foreman_version fact

--- lib/facter/foreman_version.rb
+++ lib/facter/foreman_version.rb
@@ -1,8 +1,6 @@
 Facter.add("foreman_version") do
-  setcode do
-    foreman_path = "#{Facter[:boxen_home].value}/foreman/bin/foreman"
-    if File.executable?(foreman_path)
-      Facter::Util::Resolution.exec("#{foreman_path} version 2>&1")
-    end
+  foreman_path = "#{Facter[:boxen_home].value}/foreman/bin/foreman"
+  if File.executable?(foreman_path)
+    setcode "#{foreman_path} version 2>&1"
   end
 end
